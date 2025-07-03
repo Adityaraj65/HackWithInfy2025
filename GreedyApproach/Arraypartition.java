54860294219233a412549215f5e73235f6f35a6c
Aditya <ar850732@gmail.com>
2025-07-03 10:05:15 +0530

    solved leetcode Array partition

---
+++ GreedyApproach/Arraypartition.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Arraypartition {
+    public static void main(String[] args) {
+        int[] nums={1,4,3,2};
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        System.out.println(sum);
+    }
+}
